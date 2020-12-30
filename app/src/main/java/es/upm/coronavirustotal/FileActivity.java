9f036023e9f7506706058754befca422c2a157f9
Ignacio Ripoll <ignacioripollramirez@gmail.com>
2020-12-30 20:05:16 +0100

    se muestran 6 carácteres del Hash para no saturar

--- app/src/main/java/es/upm/coronavirustotal/FileActivity.java
+++ app/src/main/java/es/upm/coronavirustotal/FileActivity.java
@@ -23,18 +23,19 @@ public class FileActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String value = intent.getStringExtra("nombre del fichero");
         TextView file_name = findViewById(R.id.file_name);
-        file_name.setText(value+"\nHash | Antivirus | Result | File Name");
+        file_name.setText(value+"\nHash MD5 | Antivirus | Result | File Name");
 
 //Acceso a la base de datos para coger los valores
         DatabaseHelper admin = new DatabaseHelper(this);
         SQLiteDatabase base_de_datos = admin.getReadableDatabase();
         String[] antivirus_results = null;
         if(base_de_datos!=null){
-            Cursor fila = base_de_datos.rawQuery("Select * from T_ANTIVIRUS where file_name='nachofile.pdf'",null);
+            Cursor fila = base_de_datos.rawQuery("Select * from T_ANTIVIRUS where file_name='dariofile.pdf'",null);
             fila.moveToFirst();
-            antivirus_results = new String[fila.getCount()*4];
-            for (int j=0, i = 0; j<fila.getCount();j++, i=i+4) {
-                antivirus_results[i]=fila.getString(0);
+            antivirus_results = new String[fila.getCount()*4]; //Por 4 ya que cada tupla de la BBDD tiene 4 columnas
+            for (int j=0, i = 0; j<fila.getCount();j++, i=i+4) { //Mas 4 para ir avanzando de tupla en tupla
+                //antivirus_results[i]=fila.getString(0);
+                antivirus_results[i]=fila.getString(0).substring(0,7);
                 antivirus_results[i+1]=fila.getString(1);
                 antivirus_results[i+2]=fila.getString(2);
                 antivirus_results[i+3]=fila.getString(3);
